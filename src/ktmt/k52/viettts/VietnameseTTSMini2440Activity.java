bdc5b94e9cfbfc612d84a0d47b8d61e66677ad1c
nguyendung.dce.hut <nguyendung.dce.hut@gmail.com>
2012-02-25 15:48:22 +0000

    Bắt đầu dự án

---
+++ src/ktmt/k52/viettts/VietnameseTTSMini2440Activity.java
@@ -0,0 +1,13 @@
+package ktmt.k52.viettts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VietnameseTTSMini2440Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
